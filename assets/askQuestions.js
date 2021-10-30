d22404b8a3a5be167d72b900ee44cb0ea73ae5d4
ggamb <glenngambardella372@gmail.com>
2021-10-30 14:33:49 -0400

    added several functions including adding department

--- assets/askQuestions.js
+++ assets/askQuestions.js
@@ -1,6 +1,7 @@
-const inquirer = require("inquirer")
-const db = require('../db/connection.js');
+const inquirer = require("inquirer");
 const cTable = require('console.table');
+const db = require('../db/connection');
+
 
 const askQuestions = () => {
     inquirer.prompt({
@@ -15,20 +16,33 @@ const askQuestions = () => {
         switch(userChoice) {
             case "View all employees":
                 viewEmployees();
+                break;
             case"View all departments":
+                viewDepartments();
+                break;
             case "View all roles":
+                viewRoles();
+                break;
             case "Add a department":
+                addDepartment();
+                break;
             case "Add a role":
+                addRole();
+                break;
             case "Add an employee":
+                addEmployee();
+                break;
             case "Update an employee role":
+                updateEmployee();
+                break;
             default:
                 console.log("Gooodbye!");
                 db.end();
-
-
+                break;
         }
     })
 }
+
 
 const viewEmployees = () => {
     const sql = 'SELECT * FROM employees';
@@ -38,8 +52,80 @@ const viewEmployees = () => {
             return;
         } else {
             console.table("Current employees", rows);
+            askQuestions();
         }
     })
 }
+
+const viewDepartments = () => {
+    const sql = 'SELECT * FROM departments';
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log("An error occurred!");
+            return;
+        } else {
+            console.table("Current departments", rows);
+            askQuestions();
+        }
+    })
+}
+
+const viewRoles = () => {
+    const sql = 'SELECT * FROM roles';
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log("An error occurred!");
+            return;
+        } else {
+            console.table("Current roles", rows);
+            askQuestions();
+        }
+    })
+}
+
+
+const addDepartment = () => {
+    inquirer.prompt({
+        message: "What is the name of the department you would like to add?",
+        type: 'input',
+        name: 'name',
+        validate: hasName => {
+            if (hasName) {
+              return true;
+            } else {
+              console.log('Please enter a name for your department!');
+              return false;
+            }
+        }
+    }).then(result => {
+        let departmentName = result.name;
+
+        const sql = 'INSERT INTO departments (name) VALUES (?)';
+        db.query(sql, departmentName, (err, rows) => {
+            if(err) {
+                console.log("An error occurred!");
+                return;
+            } else {
+                console.table("New department created!");
+                askQuestions();
+            }
+        })
+
+    })
+}
+
+
+const addRole = () => {
+    askQuestions();
+}
+
+const addEmployee = () => {
+    askQuestions();
+}
+
+const updateEmployee = () => {
+    askQuestions();
+}
+
 
 askQuestions();
